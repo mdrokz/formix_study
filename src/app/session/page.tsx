feed67443a62ea76b1dcf0c098e0650206ee8984
mdrokz <mohammadmunshi@gmail.com>
2024-01-12 23:35:56 +0530

    feat: implement UI for viewing session

---
+++ src/app/session/page.tsx
@@ -0,0 +1,10 @@
+import { Card } from "@radix-ui/themes";
+
+export default function Page() {
+
+    return (
+        <Card variant="classic" size="4" style={{ width: 400 }}>
+        </Card>
+    )
+  }
+  
