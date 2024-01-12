9f7fb0a41a7dd5c92a1001809037e0cf8184b308
Murtaza <mzrokz@live.com>
2024-01-13 02:39:19 +0530

    fix: Main page ui - wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,43 @@
+"use client";
 import Login from "@/components/login";
 import MagicLink from "@/components/magicLink";
 import SendOtp from "@/components/sendOtp";
-import { Card, TabsContent, TabsList, TabsRoot, TabsTrigger } from "@radix-ui/themes";
+import {
+  Button,
+  Card,
+  Flex,
+  Grid,
+  Heading,
+  TabsContent,
+  TabsList,
+  TabsRoot,
+  TabsTrigger,
+} from "@radix-ui/themes";
 
 export default function Home() {
-
   return (
-      <Card variant="classic" size="4" style={{ width: 400 }}>
-      <TabsRoot defaultValue="magic_link">
-        <TabsList>
-          <TabsTrigger value="magic_link">Magic Link</TabsTrigger>
-          <TabsTrigger value="send_otp" disabled>Send OTP</TabsTrigger>
-          <TabsTrigger value="verify_otp" disabled>Verify OTP</TabsTrigger>
-        </TabsList>
-        <TabsContent value="magic_link"><MagicLink/></TabsContent>
-        <TabsContent value="send_otp"><SendOtp/></TabsContent>
-        <TabsContent value="verify_otp"><Login/></TabsContent>
-      </TabsRoot>
-      </Card>
-  )
+    <Grid rows="2" className="bg-black px-40">
+      <Flex
+        direction="column"
+        justify="center"
+        align="center"
+        gap="3"
+        className="h-[100vh]"
+      >
+        <Heading>Welcome to Formix Study</Heading>
+        <h2>Description</h2>
+        <Button
+          onClick={() => {
+            const loginDiv = document.querySelector("#login");
+            if (loginDiv) {
+              loginDiv.scrollIntoView({ behavior: "smooth" });
+            }
+          }}
+        >
+          Get Started
+        </Button>
+      </Flex>
+      <Flex id="login" direction="column" className="h-[100vh]"></Flex>
+    </Grid>
+  );
 }
