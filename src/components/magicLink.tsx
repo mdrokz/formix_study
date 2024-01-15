fc29a6a108425e8cc355e63092be108b2f435429
mdrokz <mohammadmunshi@gmail.com>
2024-01-15 22:52:20 +0530

    fix: use env for auth redirect url

--- src/components/magicLink.tsx
+++ src/components/magicLink.tsx
@@ -14,7 +14,7 @@ export default function Login() {
       theme='dark'
       showLinks={false} 
       providers={[]}
-      redirectTo="http://localhost:3000/auth/callback"
+      redirectTo={process.env.NEXT_PUBLIC_AUTH_REDIRECT_URL}
     />
   )
 }
