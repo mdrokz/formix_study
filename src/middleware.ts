9163f7b1ada2eaa8c4d4cc5e907e6611cfb8da84
Murtaza <mzrokz@live.com>
2024-01-16 23:04:49 +0530

    fix: Removed path check

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,31 +1,31 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
-import { NextResponse } from 'next/server'
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
+import { NextResponse } from 'next/server';
 
-import type { NextRequest } from 'next/server'
+import type { NextRequest } from 'next/server';
 
 export async function middleware(req: NextRequest) {
-  const res = NextResponse.next()
-  const supabase = createMiddlewareClient({ req, res })
+  const res = NextResponse.next();
+  const supabase = createMiddlewareClient({ req, res });
 
   const {
     data: { user },
-  } = await supabase.auth.getUser()
+  } = await supabase.auth.getUser();
 
-  console.log(user)
+  console.log(user);
 
   // if user is signed in and the current path is / redirect the user to /upload
   if (user && req.nextUrl.pathname === '/') {
-    return NextResponse.redirect(new URL('/upload', req.url))
+    return NextResponse.redirect(new URL('/upload', req.url));
   }
 
   // if user is not signed in and the current path is not / redirect the user to /
-  if (!user && req.nextUrl.pathname !== '/') {
-    return NextResponse.redirect(new URL('/', req.url))
+  if (!user) {
+    return NextResponse.redirect(new URL('/', req.url));
   }
 
-  return res
+  return res;
 }
 
 export const config = {
   matcher: ['/', '/upload'],
-}
+};
