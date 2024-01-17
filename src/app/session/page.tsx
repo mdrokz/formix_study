66e5ed06c9c95482432122459507eb80f1b01e40
mdrokz <mohammadmunshi@gmail.com>
2024-01-17 22:36:39 +0530

    feat: display user sessions

--- src/app/session/page.tsx
+++ src/app/session/page.tsx
@@ -1,9 +1,44 @@
-import { Card } from "@radix-ui/themes";
+'use client';
+
+import { Session } from "@prisma/client";
+import { Box, Button, Card, Heading, Link } from "@radix-ui/themes";
+import { useState, useEffect } from "react";
 
 export default function Page() {
 
+    const [sessions, setSessions] = useState<Session[]>([]);
+
+    useEffect(() => {
+        fetchSessions();
+    }, [])
+
+    const fetchSessions = async () => {
+        try {
+            const res = await fetch(`/api/session`);
+
+            if (res.ok && res.status === 200) {
+                const data = await res.json();
+                setSessions(data);
+                console.log(data);
+            } else {
+                console.error('Error fetching session');
+            }
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
         <Card variant="classic" size="4" style={{ width: 400 }}>
+            <Heading>Your Sessions</Heading>
+            {sessions.map((session) => {
+                return (
+                    <Box>
+                   <Link href={`/session/${session.id}`}>{session.name}</Link>
+                    </Box>
+                )
+            })}
         </Card>
     )
   }
