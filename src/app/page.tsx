4b666decfd85419c28d617d6db57965094eb54f1
mdrokz <mohammadmunshi@gmail.com>
2024-04-27 13:21:24 +0530

    fix: disable other auth tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,12 @@ export default function Home() {
       <TabsRoot defaultValue="magic_link">
         <TabsList>
           <TabsTrigger value="magic_link">Magic Link</TabsTrigger>
-          <TabsTrigger value="send_otp" disabled>Send OTP</TabsTrigger>
-          <TabsTrigger value="verify_otp" disabled>Verify OTP</TabsTrigger>
+          {/* <TabsTrigger value="send_otp" disabled>Send OTP</TabsTrigger>
+          <TabsTrigger value="verify_otp" disabled>Verify OTP</TabsTrigger> */}
         </TabsList>
         <TabsContent value="magic_link"><MagicLink/></TabsContent>
-        <TabsContent value="send_otp"><SendOtp/></TabsContent>
-        <TabsContent value="verify_otp"><Login/></TabsContent>
+        {/* <TabsContent value="send_otp"><SendOtp/></TabsContent> */}
+        {/* <TabsContent value="verify_otp"><Login/></TabsContent> */}
       </TabsRoot>
       </Card>
   )
