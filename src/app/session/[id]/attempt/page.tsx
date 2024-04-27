531b9d5f9d613eef0f667b08ad31bc3dee687324
mdrokz <mohammadmunshi@gmail.com>
2024-04-27 15:24:29 +0530

    refactor: move attempts in session and display list of attempts

---
+++ src/app/session/[id]/attempt/page.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { Attempt } from "@prisma/client";
+import { Box, Button, Card, Heading, Link } from "@radix-ui/themes";
+import { useState, useEffect } from "react";
+
+export default function Page() {
+
+    const [attempts, setAttempts] = useState<Attempt[]>([]);
+
+    useEffect(() => {
+        fetchAttempts();
+    }, [])
+
+    const fetchAttempts = async () => {
+        try {
+            const res = await fetch(`/api/session`);
+
+            if (res.ok && res.status === 200) {
+                const data = await res.json();
+                setAttempts(data);
+                console.log(data);
+            } else {
+                console.error('Error fetching session');
+            }
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    return (
+        <Card variant="classic" size="4" style={{ width: 400 }}>
+            <Heading>Your Attempts</Heading>
+            {attempts.map((attempt, index) => {
+                return (
+                    <Box key={attempt.id}>
+                        <Link href={`/session/attempt/${attempt.id}`}>{`Attempt no. ${index}`}</Link>
+                    </Box>
+                )
+            })}
+        </Card>
+    )
+}
